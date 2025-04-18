a4854bfa94735d273c9f1e87203cef55b4e501f5
shortrounddev <collinoswalt@proton.me>
2025-04-17 21:11:10 -0400

    Added dynamic walls

---
+++ client/src/schema/ITileMessage.ts
@@ -0,0 +1,12 @@
+// 
+// THIS FILE HAS BEEN GENERATED AUTOMATICALLY
+// DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
+// 
+// GENERATED USING @colyseus/schema 3.0.33
+// 
+
+export interface ITileMessage {
+    x: number;
+    y: number;
+    type: string;
+}
